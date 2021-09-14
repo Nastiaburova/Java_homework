2de691ca6c07b42b25c62e061f92d51b7b9d50e9
Nastiaburova <31832759+Nastiaburova@users.noreply.github.com>
2021-09-14 11:53:06 +0300

    Add files via upload

---
+++ homework_3/src/main/java/Controller/Controller.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import Model.Model;
+import Model.NoteBook;
+import View.View;
+
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class Controller {
+    View view;
+    Model model;
+    public static final String REGEX_PROPP = "regex";
+    private static final ResourceBundle controllerBundle = ResourceBundle.getBundle(REGEX_PROPP,new Locale("",""));
+
+    public Controller(View view, Model model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    public void start(){
+        Scanner scanner = new Scanner(System.in);
+        NoteBook userNoteBook  = new NoteBookUtilities(this.view,scanner,this.controllerBundle).note();
+    }
+}
+
---
+++ homework_3/src/main/java/View/View.java
@@ -0,0 +1,28 @@
+package View;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class View {
+    public final static String BUNNDLE_NAME = "messages";
+    public final static String SPACE_LITTERAL = " ";
+    public final ResourceBundle bundle = ResourceBundle.getBundle(BUNNDLE_NAME,new Locale("",""));
+
+    public String messageBuilder(String... strings){
+        StringBuilder result = new StringBuilder();
+        for (String message: strings) {
+            result.append(message + SPACE_LITTERAL);
+        }
+        return result.toString();
+    }
+    public void printInputMessage(String message){
+        System.out.println(bundle.getString(TextConstants.INPUT_STRING_DATA));
+        System.out.println(message);
+    }
+    public void printWrondDataMessage(String message){
+        System.out.println(bundle.getString(TextConstants.WRONG_INPUT_DATA));
+        System.out.println(message);
+
+    }
+
+}
